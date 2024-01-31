3a6451adab61c2b681749bd507634996b9eec37e
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-31 11:16:08 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by GengYu Zhang on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
